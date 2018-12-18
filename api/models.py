ff8405f12c877020b82ee14e87bce48a9327435d
Edwin Velasco <edwinalbertovelasco8@gmail.com>
2018-12-18 17:50:23 -0500

    commit init

---
+++ api/models.py
@@ -0,0 +1,25 @@
+# from django.db import models
+
+# Create your models here.
+from djongo import models
+
+class Document(models.Model):
+    # auto update when data is inserted
+    create_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    # auto update when data is altered
+    update_at = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    title = models.CharField(max_length=45, null=True)
+    slug = models.SlugField(max_length=100)
+    doc_type = models.CharField(max_length=25)
+    period = models.CharField(max_length=5)
+    student = models.CharField(max_length=8)
+
+    class Meta:
+        verbose_name = 'Documento'
+        verbose_name_plural = 'Documentos'
+        ordering = ('create_at',)
+        db_table = 'api_documents'
+
+    def __str__(self):
+        return self.title
